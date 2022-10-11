a03bd646c696284ce5db2492247ed2c7f96a2582
RajeshPolarati <87992930+RajeshPolarati@users.noreply.github.com>
2022-10-11 13:15:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { useEffect } from "react";
-import { isReady } from "snarkyjs";
+import { isReady, PublicKey } from "snarkyjs";
 import { SimpleZkapp_ } from "zkapp-snarkyjs";
 function App() {
   useEffect(()=>{
     (async()=>{
       await isReady
       await SimpleZkapp_.compile()
+      const contract = new SimpleZkapp_(PublicKey.fromBase58('B62qkz3eEeD5XHDEMLubSo4YQ6Xejs6tnh6sx3CYtGCXEEA1DaNc4cq'))
+      let val = contract.value.get()
+      console.log(`Found deployed zkapp, with state ${val.toBase58()}`);
     })()
   },[])
   return (
