ab4723ce88e0f7841aeb8d1044a379c2e70a7be7
RajeshPolarati <87992930+RajeshPolarati@users.noreply.github.com>
2022-10-11 22:29:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
 import { useEffect } from "react";
-import { fetchAccount, Field, isReady, Mina, PublicKey, setGraphqlEndpoint,PrivateKey } from "snarkyjs";
+import { fetchAccount, Field, isReady, Mina, PrivateKey, PublicKey, setGraphqlEndpoint } from "snarkyjs";
 // import { SimpleZkapp_ } from "zkapp-snarkyjs";
 import { Add } from "zkproject";
-
 function App() {
+  const initalize = async() =>{
+    await isReady
+    const graphqlEndpoint = 'https://proxy.berkeley.minaexplorer.com/graphql';
+    setGraphqlEndpoint(graphqlEndpoint);
+    let Berkeley = Mina.BerkeleyQANet(graphqlEndpoint);
+    Mina.setActiveInstance(Berkeley);
+  }
+  const createTransaction = async(contract) =>{
+    let feePayer = PrivateKey.fromBase58('EKEeaxiDz6utkQBKApPXLd6z3XsY9Nz1kgygKLcDVb9tN4HbY89U')
+        let txn = await Mina.transaction(feePayer, () => {
+          console.log('inside transaction');
+          contract.init();
+        });
+  }
   useEffect(()=>{
     (async()=>{
-      await isReady
-      console.log("inside async");
-      const graphqlEndpoint = 'https://proxy.berkeley.minaexplorer.com/graphql';
-      setGraphqlEndpoint(graphqlEndpoint);
-      let Berkeley = Mina.BerkeleyQANet(graphqlEndpoint);
-      Mina.setActiveInstance(Berkeley);
+      await initalize();
       const zkAppAddress = 'B62qosyTfXPG88qRH3K6MYAvevaQt8GcXsG4m9BwQEjNDf4KRazjv89'
       let {account,error} = await fetchAccount({ publicKey: PublicKey.fromBase58(zkAppAddress)})
       console.log(account);
       console.log(error)
       await Add.compile()
       // await SimpleZkapp_.compile()
+      console.log("in try");
+      const contract = new Add(PublicKey.fromBase58(zkAppAddress))
+      await createTransaction(contract)
+        
       try{
-        console.log("in try");
-       const contract = new Add(PublicKey.fromBase58(zkAppAddress))
-      //contract.init()
+
       console.log(contract);
       console.log("after contract");
-        let feePayer = PrivateKey.fromBase58('EKEeaxiDz6utkQBKApPXLd6z3XsY9Nz1kgygKLcDVb9tN4HbY89U')
-        let txn = await Mina.transaction(feePayer, () => {
-          contract.init();
-        });
-        console.log(contract.num)
-
+      console.log(contract.num)
       let val = contract.num.get()
       console.log("val",val);
       console.log(`Found deployed zkapp, with state ${val.toBigInt()}`);
@@ -41,7 +46,7 @@ function App() {
   },[])
   return (
     <div className="App">
-      <h1>React app hello</h1>
+      <h1>React app hell0o</h1>
     </div>
   );
 }
