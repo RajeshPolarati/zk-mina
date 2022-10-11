4cbfdb7c793865413b9f6940d1923ac1224f9267
RajeshPolarati <87992930+RajeshPolarati@users.noreply.github.com>
2022-10-11 14:13:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { useEffect } from "react";
-import { fetchAccount, isReady, PublicKey } from "snarkyjs";
+import { fetchAccount, isReady, Mina, PublicKey, setGraphqlEndpoint } from "snarkyjs";
 import { SimpleZkapp_ } from "zkapp-snarkyjs";
+
 function App() {
   useEffect(()=>{
     (async()=>{
       await isReady
       console.log("inside async");
-      let {account,error} = await fetchAccount({ publicKey: PublicKey.fromBase58('B62qoD7GZfMURQSpEF98HBTCuuchzgiw43dNsZXanMg8w6AYKLCuVfc')})
+      const graphqlEndpoint = 'https://proxy.berkeley.minaexplorer.com/graphql';
+      setGraphqlEndpoint(graphqlEndpoint);
+      let Berkeley = Mina.BerkeleyQANet(graphqlEndpoint);
+      Mina.setActiveInstance(Berkeley);
+      const zkAppAddress = 'B62qiY3PVYcjdDfQehgSJDgKUsGbg5WwkEeD26Jb9ZxExRQ4xjW1AMg'
+      let {account,error} = await fetchAccount({ publicKey: PublicKey.fromBase58(zkAppAddress)})
       console.log(account);
       await SimpleZkapp_.compile()
       try{
-        const contract = new SimpleZkapp_(PublicKey.fromBase58('B62qoD7GZfMURQSpEF98HBTCuuchzgiw43dNsZXanMg8w6AYKLCuVfc'))
+        const contract = new SimpleZkapp_(PublicKey.fromBase58(zkAppAddress))
       let val = contract.value.get()
       console.log(`Found deployed zkapp, with state ${val.toBase58()}`);
       }catch(error){
